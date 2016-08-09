ef780233b3c9b24b67da546ddd9d932fe9ce1d88
Andreas Neumann <andr.neumann@googlemail.com>
2016-08-09 16:14:19 +0200

    Interpolation fortgesetzt (tbc)

--- install/lib/interpolation.py
+++ install/lib/interpolation.py
@@ -82,112 +82,45 @@ def interpol(start,end,line,intertype):
 		cursor3.execude("INSERT INTO planet_osm_point (\"addr:housenumber\", \"addr:street\", \"addr:suburb\", \"addr:city\", \"addr:postcode\", way) VALUES (%s,%s,%s,%s,%s,ST_Line_Interpolate_Point(st_linefromtext(%s,4326),%s)),"
                     ,(str(start['housenumber']+i), start['street'], start['suburb'], start['city'], start['postcode'], 'LINESTRING('+','.join(line)+')',str($i/$anzahl1)))
 		i++
-    elif((intertype=='even') or (intertype=='odd')) {
-	if($start['housenumber']>$end['housenumber']) {
-				$anzahl = ($start['housenumber'] - $end['housenumber'])/2;
-				$anzahl1 = $anzahl;
-				$i=1;
-				for($anzahl;$anzahl>1;$anzahl--) {
-					$query = "INSERT INTO planet_osm_point (\"addr:housenumber\", \"addr:street\", \"addr:suburb\", \"addr:city\", \"addr:postcode\", way) VALUES ('".
-					($start['housenumber']-($i*2))."','".
-					$start['street']."','".
-					$start['suburb']."','".
-					$start['city']."','".
-					$start['postcode']."',".
-					"ST_Line_Interpolate_Point(st_linefromtext('LINESTRING(".implode(',',$line).")',4326),".$i/$anzahl1."))"; 
-					pg_query($query);
-					$i++;
-				}
-			}
-			else {
-				$anzahl = ($end['housenumber'] - $start['housenumber'])/2;
-				$anzahl1 = $anzahl;
-				$i=1;
-				for($anzahl;$anzahl>1;$anzahl--) {
-					$query = "INSERT INTO planet_osm_point (\"addr:housenumber\", \"addr:street\", \"addr:suburb\", \"addr:city\", \"addr:postcode\", way) VALUES ('".
-					($start['housenumber']+($i*2))."','".
-					$start['street']."','".
-					$start['suburb']."','".
-					$start['city']."','".
-					$start['postcode']."',".
-					"ST_Line_Interpolate_Point(st_linefromtext('LINESTRING(".implode(',',$line).")',4326),".$i/$anzahl1."))"; 
-					pg_query($query);
-					$i++;
-				}
-			}
-		}
-		elseif($intertype=='alphabetic') {
-			$number = array(
-				1 => 'a',
-				2 => 'b',
-				3 => 'c',
-				4 => 'd',
-				5 => 'e',
-				6 => 'f',
-				7 => 'g',
-				8 => 'h',
-				9 => 'i',
-				10 => 'j',
-				11 => 'k',
-				12 => 'l',
-				13 => 'm',
-				14 => 'n',
-				16 => 'o',
-				17 => 'p',
-				18 => 'q',
-				19 => 'r',
-				20 => 's',
-				21 => 't',
-				22 => 'v',
-				23 => 'w',
-				24 => 'x',
-				25 => 'y',
-				26 => 'z'
-				); // 
-			$alpha = array_flip($number);
-			preg_match('/([a-zA-Z])/',$start['housenumber'],$match_start);
-			preg_match('/([a-zA-Z])/',$end['housenumber'],$match_end);
-			preg_match('/([0-9]*)/',$start['housenumber'],$match_start_nr);
-			preg_match('/([0-9]*)/',$end['housenumber'],$match_end_nr);
-			if($match_end_nr[1]==$match_start_nr[1]) {
-				$anzahl = $alpha[strtolower($match_start[1])]-$alpha[strtolower($match_end[1])];
-				if($alpha[strtolower($match_start[1])]>$alpha[strtolower($match_end[1])]) {
-					$anzahl = $alpha[strtolower($match_start[1])]-$alpha[strtolower($match_end[1])];
-					$anzahl1 = $anzahl;
-					$i=1;
-					for($anzahl;$anzahl>1;$anzahl--) {
-						$query = "INSERT INTO planet_osm_point (\"addr:housenumber\", \"addr:street\", \"addr:suburb\", \"addr:city\", \"addr:postcode\", way) VALUES ('".
-						($match_start_nr[1].$number[$alpha[strtolower($match_start[1])]-$i])."','".
-						$start['street']."','".
-						$start['suburb']."','".
-						$start['city']."','".
-						$start['postcode']."',".
-						"ST_Line_Interpolate_Point(st_linefromtext('LINESTRING(".implode(',',$line).")',4326),".$i/$anzahl1."))"; 
-						pg_query($query);
-						$i++;
-					}
-				}
-				else {
-					$anzahl = $alpha[strtolower($match_end[1])]-$alpha[strtolower($match_start[1])];
-					$anzahl1 = $anzahl;
-					$i=1;
-					for($anzahl;$anzahl>1;$anzahl--) {
-						$query = "INSERT INTO planet_osm_point (\"addr:housenumber\", \"addr:street\", \"addr:suburb\", \"addr:city\", \"addr:postcode\", way) VALUES ('".
-						($match_start_nr[1].$number[$alpha[strtolower($match_start[1])]+$i])."','".
-						$start['street']."','".
-						$start['suburb']."','".
-						$start['city']."','".
-						$start['postcode']."',".
-						"ST_Line_Interpolate_Point(st_linefromtext('LINESTRING(".implode(',',$line).")',4326),".$i/$anzahl1."))"; 
-						pg_query($query);
-						$i++;
-					}
-				}
-				
-			}
-			//exit;
-		}
-	}
+    elif((intertype=='even') or (intertype=='odd'))
+	if(start['housenumber']>end['housenumber'])
+            anzahl = (start['housenumber'] - end['housenumber'])/2;
+            anzahl1 = anzahl;
+            i=1;
+            for(anzahl;anzahl>1;anzahl--)
+		cursor3.execude("INSERT INTO planet_osm_point (\"addr:housenumber\", \"addr:street\", \"addr:suburb\", \"addr:city\", \"addr:postcode\", way) VALUES (%s,%s,%s,%s,%s,ST_Line_Interpolate_Point(st_linefromtext(%s,4326),%s)),"
+                    ,(str(start['housenumber']-(i*2)), start['street'], start['suburb'], start['city'], start['postcode'], 'LINESTRING('+','.join(line)+')',str($i/$anzahl1)))
+		i++
+	else
+            anzahl = (end['housenumber'] - start['housenumber'])/2;
+            anzahl1 = anzahl;
+            i=1;
+            for(anzahl;anzahl>1;anzahl--)
+		cursor3.execude("INSERT INTO planet_osm_point (\"addr:housenumber\", \"addr:street\", \"addr:suburb\", \"addr:city\", \"addr:postcode\", way) VALUES (%s,%s,%s,%s,%s,ST_Line_Interpolate_Point(st_linefromtext(%s,4326),%s)),"
+                    ,(str(start['housenumber']+(i*2)), start['street'], start['suburb'], start['city'], start['postcode'], 'LINESTRING('+','.join(line)+')',str($i/$anzahl1)))
+		i++
+                
+    elif(intertype=='alphabetic')
+        number = re.findall(r'\d+',start['housenumber'])
+	if(number==re.findall(r'\d+',end['housenumber']))
+            end_char = re.findall(r'[a-z]+',lower(end['housenumber']))
+            start_char = re.findall(r'[a-z]+',lower(start['housenumber']))
+            if(len(start_char) === len(end_char) === 1)
+                anzahl = ord(end_char)-ord(start_char)
+                if(anzahl > 0)
+                    anzahl1 = anzahl;
+                    i=1;
+                    for(anzahl;anzahl>1;anzahl--)
+                        cursor3.execude("INSERT INTO planet_osm_point (\"addr:housenumber\", \"addr:street\", \"addr:suburb\", \"addr:city\", \"addr:postcode\", way) VALUES (%s,%s,%s,%s,%s,ST_Line_Interpolate_Point(st_linefromtext(%s,4326),%s)),"
+                            ,(str(number)+chr(start_char-i)), start['street'], start['suburb'], start['city'], start['postcode'], 'LINESTRING('+','.join(line)+')',str($i/$anzahl1)))
+                        i++
+                elif(anzahl<0)
+                    anzahl1 = anzahl;
+                    i=1;
+                    for(anzahl;anzahl>1;anzahl--)
+                        cursor3.execude("INSERT INTO planet_osm_point (\"addr:housenumber\", \"addr:street\", \"addr:suburb\", \"addr:city\", \"addr:postcode\", way) VALUES (%s,%s,%s,%s,%s,ST_Line_Interpolate_Point(st_linefromtext(%s,4326),%s)),"
+                            ,(str(number)+chr(start_char+i)), start['street'], start['suburb'], start['city'], start['postcode'], 'LINESTRING('+','.join(line)+')',str($i/$anzahl1)))
+                        i++
 
 cursor.execute("SELECT * FROM planet_osm_rels")
 
@@ -312,35 +245,3 @@ while interpolation:
         else:
             linepoints.append(coordinate)
     interpolation = cursor.fetchone()
-
-#                        //Punktabfrage
-#                        $result2 = pg_query("select \"addr:housenumber\" as housenumber,\"addr:postcode\" as postcode,\"addr:street\" as street,\"addr:city\" as city,\"addr:suburb\" as suburb, st_astext(way) as koord from planet_osm_point where \"addr:housenumber\" IS NOT NULL AND way = st_pointfromtext('POINT(".$a.")',4326)");
-#                        if(pg_num_rows($result2)==1) {
-#                                $point = pg_fetch_assoc($result2);
-#                                //Wenn bereits Startwert festgelegt & Punkt wichtig
-#                                if((count($start)>0)&&(is_inter_point($point['housenumber'],$interpolation['inter']))) {
-#                                        //Punkt hinzufügen
-#                                        $linepoints[] = $a;
-#                                        //Interpolieren
-#                                        interpol($start,$point,$linepoints,$interpolation['inter']);
-#                                        // Neuen Startwert festlegen
-#                                        $start = $point;
-#                                        //Liniencache leeren
-#                                        $linepoints = array($a);
-#                                }
-#                                elseif((count($start)==0)&&(is_inter_point($point['housenumber'],$interpolation['inter']))) {
-#                                        //Startwert festlegen
-#                                        $start = $point;
-#                                        //Liniencache leeren
-#                                        $linepoints = array($a);
-#                                }
-#                                else { //unwichtiger Punkt
-#                                        $linepoints[] = $a;
-#                                }
-#                        }
-#                        else {
-#                                $linepoints[] = $a;
-#                        }
-#                }
-#        }
-#}
